c3fa6fa39246968a751c15f6444eb5e051a45e83
Kamil Gałuszka <galuszkak@gmail.com>
2014-03-07 23:23:40 +0100

    updates to routers

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,7 +2,9 @@ var views = require("../controllers/home");
 var Router = require('koa-router');
 var router = new Router();
 
+router.get("home-page", "/", views.homePage);
+router.get("admin-page", "/admin_panel/", views.adminPage)
+
 exports = router;
 
-router.get("/", views.homePage);
-router.get("/admin_panel/", views.adminPage)
+
