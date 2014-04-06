422be9e23012bc456539e9b07104095435434e7f
Kamil Gałuszka <galuszkak@gmail.com>
2014-04-06 08:14:04 +0200

    added missing files

---
+++ server/controllers/admin/offerController.js
@@ -0,0 +1,12 @@
+var views = require('co-views'),
+  Offer = require('../../models/offer.js');
+
+function * offerList() {
+
+  this.body = yield Offer.find().sort('-title').exec();
+
+}
+
+module.exports = {
+    offerList: offerList,
+}
---
+++ test/models/subscription.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose'),
+  mongooseConfig = require('../../server/config/mongoose'),
+  Subscription = require('../../server/models/offer.js').Subscription,
+  connection = mongoose.connection,
+  chance = new require('chance');
+
+describe('Subscription model', function () {
+
+  it("should create Subscription object", function (done) {
+    done();
+  });
+  
+  it("should find Subscription object", function (done) {
+    done();
+  });
+
+  it("should update Subscription object", function (done) {
+    done();
+  });
+  
+  it("should delete Subscription object", function (done) {
+    done();
+  });
+  
+});
+
