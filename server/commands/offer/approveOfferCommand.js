9b56239ce7e1d112642954025b2aea2c93578d8f
piotrekbass <piotrek.koscielniak@gmail.com>
2014-05-27 23:47:10 +0200

    approveOfferCommand added validation

--- server/commands/offer/approveOfferCommand.js
+++ server/commands/offer/approveOfferCommand.js
@@ -1,11 +1,19 @@
+"use strict";
 
-function validate(){
+var offerRepository = require('../../repositories/offerRepository');
+
+function validate(params) {
+  if (!params.offerId) {
+    return [
+      {field: "offerId", message: "Id oferty jest wymagane", ruleName: "required"}
+    ];
+  }
   return null;
 }
 
 
-function * execute() {
-
+function * execute(params) {
+//  var offer = yield offerRepository.findOne({_id: params.offerId});
 }
 
 module.exports.execute = execute;
--- test/commands/offer/approveOfferCommand.test.js
+++ test/commands/offer/approveOfferCommand.test.js
@@ -1,8 +1,29 @@
 "use strict";
 
 var expect = require('chai').expect;
+var co = require('co');
+var commandInvoker = require('../../../server/commands/commandInvoker');
+var approveOfferCommand = require('../../../server/commands/offer/approveOfferCommand');
 
 describe("approveOfferCommand", function () {
+
+  var invokerParams = {
+    command: approveOfferCommand
+  };
+
+  it("should return errors when called with invalid params", function (done) {
+    invokerParams.commandParams = {};
+
+    co(function * () {
+      var result = yield commandInvoker.invoke(invokerParams);
+
+      expect(result.status).to.equal(false);
+      expect(result.errors).to.be.ok;
+      expect(result.errors[0].field).to.equal("offerId");
+      expect(result.errors[0].message).to.equal("Id oferty jest wymagane");
+      expect(result.errors[0].ruleName).to.equal("required");
+    })(done);
+  });
 
   it("should approve offer", function () {
 
